4d90ac164e25647c8426f491a3f8e8a8e3f19cfb
Mike <mike@cannstandard.om>
2023-08-02 18:45:21 -0400

    switch to json instead of text so we can chunk by time

--- chunker.py
+++ chunker.py
@@ -3,22 +3,34 @@ from jinja2 import Template
 from pathlib import Path
 import json
 
-def chunker(filename, chunk_size = 1024*4, chunk_overlap = 0):
-    from langchain.text_splitter import RecursiveCharacterTextSplitter
+def whisper_chunker(filename, chunk_size = 300):
+    lines = json.load(open(filename))
+    texts = []
 
-    lines = open(filename).readlines()
-    clean_text = '\n'.join([x.strip() for x in lines])
+    text = ''
+    start_time = None
 
-    # Split the input into overlapping chunks
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap, length_function=len)
-    texts = text_splitter.split_text(clean_text)
-    print("Input text",len(clean_text),"characters, split into",len(texts),"chunks")
+    if 'transcription' in lines:
+        sections = [ { 'text': x['text'], 'from': x['timestamps']['from']/1000.0, 'to': x['timestamps']['to']/1000.0  } for x in lines['transcription'] ]
+    elif 'segments' in lines:
+        sections = [ { 'text': x['text'], 'from': x['start'], 'to': x['end']  } for x in lines['segments'] ]
+    else:
+        raise Exception('No transcription or segments found')
+    
+    for section in sections:
+        text += section['text']
+        if start_time is None: start_time = section['from']
+        if section['to'] - start_time > chunk_size:
+            texts.append(text)
+            text = ''
+            start_time = section['from']
+    texts.append(text)
 
     return texts
 
-def main(text: str, template: str, chunk_size: int = 1024*4, chunk_overlap: int = 0):
+def main(text: str, template: str, chunk_size: int = 300):
     the_template = Template(open(template).read())
-    texts = chunker(text, chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    texts = whisper_chunker(text, chunk_size=chunk_size)
 
     prepare = []
     for idx, chunk in enumerate(texts):
@@ -26,9 +38,9 @@ def main(text: str, template: str, chunk_size: int = 1024*4, chunk_overlap: int 
         item = { 'language': 'english', 'name': f'chunk-{idx}', 'prompt': prompt }
         prepare.append(item)
 
-    text_clean = text.replace('.','-').replace('_','-')
+    text_clean = Path(template).stem.replace('.','-').replace('_','-')
     templateout_name = Path(template).stem 
-    outfn = f'prepare_{text_clean}-{chunk_size}_english_{templateout_name}.ndjson'
+    outfn = f'results/prepare_{text_clean}-{chunk_size}_english_{templateout_name}.ndjson'
     open(outfn,'w').write('\n'.join([json.dumps(x) for x in prepare]))
     print('Wrote',len(prepare),'items to',outfn)
 
