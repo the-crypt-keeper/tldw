0b0e1dd3e7a8e63e3eb58ed26cbd04646e825905
Robert <contact@rmusser.net>
2024-08-01 12:42:41 -0700

    UI Fixups

--- App_Function_Libraries/Gradio_Related.py
+++ App_Function_Libraries/Gradio_Related.py
@@ -591,6 +591,8 @@ def create_video_transcription_tab():
         with gr.Row():
             gr.Markdown("""Follow this project at [tldw - GitHub](https://github.com/rmusser01/tldw)""")
         with gr.Row():
+            gr.Markdown("""If you're wondering what all this is, please see the 'Introduction/Help' tab up above for more detailed information and how to obtain an API Key.""")
+        with gr.Row():
             with gr.Column():
                 url_input = gr.Textbox(label="URL(s) (Mandatory)",
                                        placeholder="Enter video URLs here, one per line. Supports YouTube, Vimeo, other video sites and Youtube playlists.",
