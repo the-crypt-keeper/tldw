de888008540be2ed72e78a7798aa8d631e646bce
Robert <contact@rmusser.net>
2024-09-10 22:24:25 -0700

    Update Media_Wiki.py

--- App_Function_Libraries/MediaWiki/Media_Wiki.py
+++ App_Function_Libraries/MediaWiki/Media_Wiki.py
@@ -38,15 +38,12 @@ def setup_logger(name: str, level: int = logging.INFO, log_file: Optional[str] =
 
     return logger
 
-
 # Usage
 logger = setup_logger('mediawiki_import', log_file='mediawiki_import.log')
 
-
-
 # Load configuration
 def load_mediawiki_import_config():
-    with open('Config_Files\\mediawiki_import_config.yaml', 'r') as f:
+    with open(os.path.join('Config_Files', 'mediawiki_import_config.yaml'), 'r') as f:
         return yaml.safe_load(f)
 
 
