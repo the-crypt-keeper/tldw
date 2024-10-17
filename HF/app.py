567b30dfb0eb1736408670f575bd1ef6bc5aa0f7
Robert <contact@rmusser.net>
2024-10-16 18:10:33 -0700

    Local llamafile fixes

--- HF/app.py
+++ HF/app.py
@@ -15,7 +15,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'App_Fun
 from App_Function_Libraries.Books.Book_Ingestion_Lib import ingest_folder, ingest_text_file
 from App_Function_Libraries.Chunk_Lib import  semantic_chunk_long_file#, rolling_summarize_function,
 from App_Function_Libraries.Gradio_Related import launch_ui
-from App_Function_Libraries.Local_LLM_Inference_Engine_Lib import cleanup_process, local_llm_function
+from App_Function_Libraries.Local_LLM.Local_LLM_Inference_Engine_Lib import cleanup_process, local_llm_function
 from App_Function_Libraries.Summarization.Local_Summarization_Lib import summarize_with_local_llm
 from App_Function_Libraries.Summarization.Summarization_General_Lib import summarize_with_openai, summarize_with_anthropic, \
     summarize_with_cohere, summarize_with_groq, perform_transcription, perform_summarization
