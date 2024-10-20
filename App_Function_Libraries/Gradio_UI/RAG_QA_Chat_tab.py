681bbd0fe60e5e545b3b0aa7272dfa33746c75b8
Robert <contact@rmusser.net>
2024-10-20 16:17:22 -0700

    And now we have re-ranking + top_k

--- App_Function_Libraries/Gradio_UI/RAG_QA_Chat_tab.py
+++ App_Function_Libraries/Gradio_UI/RAG_QA_Chat_tab.py
@@ -73,6 +73,7 @@ def create_rag_qa_chat_tab():
                 top_k_input = gr.Number(value=10, label="Maximum amount of results to use (Default: 10)", minimum=1, maximum=50, step=1, precision=0, interactive=True)
                 keywords_input = gr.Textbox(label="Keywords (comma-separated) to filter results by)", visible=True)
                 use_query_rewriting = gr.Checkbox(label="Use Query Rewriting", value=True)
+                use_re_ranking = gr.Checkbox(label="Use Re-ranking", value=True)
                 # with gr.Row():
                 #     page_number = gr.Number(value=1, label="Page", precision=0)
                 #     page_size = gr.Number(value=20, label="Items per page", precision=0)
@@ -385,7 +386,7 @@ Rewritten Question:"""
 
         def rag_qa_chat_wrapper(message, history, context_source, existing_file, search_results, file_upload,
                                 convert_to_text, keywords, api_choice, use_query_rewriting, state_value,
-                                keywords_input, top_k_input):
+                                keywords_input, top_k_input, use_re_ranking):
             try:
                 logging.info(f"Starting rag_qa_chat_wrapper with message: {message}")
                 logging.info(f"Context source: {context_source}")
@@ -421,8 +422,8 @@ Rewritten Question:"""
 
                 if context_source == "All Files in the Database":
                     # Use the enhanced_rag_pipeline to search the entire database
-                    context = enhanced_rag_pipeline(rephrased_question, api_choice, keywords=keywords_input,
-                                                    top_k=int(top_k_input))
+                    context = enhanced_rag_pipeline(rephrased_question, api_choice, keywords_input, top_k_input,
+                                                    use_re_ranking)
                     logging.info(f"Using enhanced_rag_pipeline for database search")
                 elif context_source == "Search Database":
                     context = f"media_id:{search_results.split('(ID: ')[1][:-1]}"
