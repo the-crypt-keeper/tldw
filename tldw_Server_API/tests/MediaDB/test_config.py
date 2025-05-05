19204233dd0150e05879db62c745d0d348fcf1e8
Robert <contact@rmusser.net>
2025-05-04 23:47:03 -0700

    Completely removing MediaDB v1

--- tldw_Server_API/tests/MediaDB/test_config.py
+++
@@ -1,9 +0,0 @@
-# tests/test_config.py
-from tldw_Server_API.app.core.Utils.Utils import load_comprehensive_config
-
-def test_load_comprehensive_config():
-    config = load_comprehensive_config()
-    assert 'Database' in config
-    assert 'sqlite_path' in config['Database']
-    assert 'API' in config
-    assert 'openai_api_key' in config['API']
